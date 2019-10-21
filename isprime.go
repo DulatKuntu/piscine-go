06e152b2a1a5527df2b5fb3523bacb5f1fb8e108
DulatKuntu <dulaizzhambula@gmail.com>
2019-10-22 00:22:38 +0600

    lala

--- isprime.go
+++ isprime.go
@@ -3,9 +3,9 @@ package piscine
 func IsPrime(nb int) bool {
 	if nb == 1 {
 		return false
-	} else if nb == 2 || nb == 3 || nb == 5 || nb == 7 {
+	} else if nb == 2 || nb == 3 || nb == 5 || nb == 7 || nb == 11 || nb == 13 {
 		return true
-	} else if nb%2 == 0 || nb%3 == 0 || nb%5 == 0 || nb%7 == 0 || nb%13 == 0 {
+	} else if nb%2 == 0 || nb%3 == 0 || nb%5 == 0 || nb%7 == 0 || nb%11 == 0 || nb%13 == 0 {
 		return false
 	} else {
 		return true
