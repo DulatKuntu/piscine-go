cb78859e9bb1f708e4c90e3812ae47c125775a4b
DulatKuntu <dulaizzhambula@gmail.com>
2019-10-21 23:19:52 +0600

    lala

---
+++ recursivepower.go
@@ -0,0 +1,13 @@
+package piscine
+
+func RecursivePower(nb int, power int) int {
+	result := nb
+	if power < 0 {
+		return 0
+	} else if power == 0 {
+		return 1
+	} else {
+		result = RecursivePower(nb, power-1)
+		return result
+	}
+}
