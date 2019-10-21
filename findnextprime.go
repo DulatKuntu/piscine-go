b957e46e60de9b72f372e192de311f2cf7c06bb2
DulatKuntu <dulaizzhambula@gmail.com>
2019-10-22 00:55:25 +0600

    lal

--- findnextprime.go
+++ findnextprime.go
@@ -1,19 +1,17 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	nextprime := false
-	for i := nb; ; i++ {
-		if i == 1 {
-			nextprime = false
-		} else if i == 2 || i == 3 || i == 5 || i == 7 || i == 79 {
-			nextprime = true
-		} else if i%2 == 0 || i%3 == 0 || i%5 == 0 || i%7 == 0 || i%13 == 0 || i%79 == 0 {
-			nextprime = false
+	if nb < 2 {
+		return 2
+	} else {
+		if IsPrime(nb) == true {
+			return nb
 		} else {
-			nextprime = true
-		}
-		if nextprime == true {
-			return i
+			for i := nb; ; i++ {
+				if IsPrime(i) == true {
+					return i
+				}
+			}
 		}
 	}
 }
