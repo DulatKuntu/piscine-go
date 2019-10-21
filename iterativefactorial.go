0b3fd9741a626e1e96391182ba17e88a3f3a8dd9
DulatKuntu <dulaizzhambula@gmail.com>
2019-10-21 22:58:25 +0600

    lala

---
+++ iterativefactorial.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	if nb > 0 {
+		var result int = 1
+		for i := 1; i <= nb; i++ {
+			result = result * i
+		}
+		return result
+	} else if nb == 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
