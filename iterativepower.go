4bc147dc30046803f856072ae6cc485ec711550c
DulatKuntu <dulaizzhambula@gmail.com>
2019-10-21 23:11:52 +0600

    lala

--- iterativepower.go
+++ iterativepower.go
@@ -1,12 +1,12 @@
-package piscine 
+package piscine
 
 func IterativePower(nb int, power int) int {
 	if power < 0 {
 		return 0
 	}
 	result := nb
-	for i := 1; i <= power - 1; i++ {
-		result = nb *  result
+	for i := 1; i <= power-1; i++ {
+		result = nb * result
 	}
 	return result
 }
