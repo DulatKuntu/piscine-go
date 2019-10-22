537b95e33ce22fb4d3d4f1656e48e556b95dcc81
DulatKuntu <dulaizzhambula@gmail.com>
2019-10-22 19:11:27 +0600

    lala

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,27 +3,25 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
-	if n > 0 {
-		var byt [30]int
-		counter := 0
-		for i := 0; n > 0; i++ {
-			byt[i] = n % 10
-			n = n / 10
-			counter++
-			if n == 0 {
-				break
+	if n < 0 {
+		return
+	} else if n == 0 {
+		z01.PrintRune(rune(0))
+		return
+	}
+	var strrune = []rune{' '}
+	for i := 0; n != 0; i++ {
+		strrune = append(strrune, rune(n%10+48))
+		n = n / 10
+	}
+	for index := range strrune {
+		for j := range strrune {
+			if strrune[j] > strrune[index] {
+				strrune[j], strrune[index] = strrune[index], strrune[j]
 			}
 		}
-		min := byt[0]
-		for i := 0; i <= counter; i++ {
-			for j := i; j <= counter; j++ {
-				if byt[j] < min {
-					n = min
-					min = byt[j]
-					byt[j] = n
-				}
-			}
-			z01.PrintRune(rune(min))
-		}
+	}
+	for index := range strrune {
+		z01.PrintRune(strrune[index])
 	}
 }
