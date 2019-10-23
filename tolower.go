6f5e13925592513a25f257181d35fa7a7a80cd2d
DulatKuntu <dulaizzhambula@gmail.com>
2019-10-23 16:43:20 +0600

    lala

---
+++ tolower.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ToLower(s string) string {
+	var str string
+	for _, letter := range s {
+		if letter > 64 && letter < 91 {
+			str = str + string(letter + 32)
+		} else {
+			str = str + string(letter)
+		}
+	}
+	return str
+}
