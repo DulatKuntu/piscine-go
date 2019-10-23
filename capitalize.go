e82d95327a8e9b9b89b5676d39def806d33f0eed
DulatKuntu <dulaizzhambula@gmail.com>
2019-10-23 17:22:14 +0600

    lakl

--- capitalize.go
+++ capitalize.go
@@ -2,19 +2,20 @@ package piscine
 
 func Capitalize(s string) string {
 	var str string
-	toUpper := false
+	toUpper := true
 	for _, letter := range s {
 		if (letter > 96 && letter < 123) || (letter > 64 && letter < 91) || (letter > 47 && letter < 58) {
-			if toUpper {
-				if letter > 96 && letter < 123 {
-					str = str + string(letter-32)
-					toUpper = false
-				} else {
-					str = str + string(letter)
-					toUpper = false
-				}
+			if (letter > 96 && letter < 123) && toUpper {
+				str = str + string(letter-32)
+				toUpper = false
+			} else if (letter > 64 && letter < 91) && toUpper {
+				str = str + string(letter)
+				toUpper = false
+			} else if (letter > 64 && letter < 91) && !toUpper {
+				str = str + string(letter+32)
 			} else {
 				str = str + string(letter)
+				toUpper = false
 			}
 		} else {
 			str = str + string(letter)
