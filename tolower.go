4fb9ac8f68c5b9702d5505bf23fa571b139488e7
DulatKuntu <dulaizzhambula@gmail.com>
2019-10-23 16:44:21 +0600

    lala

--- tolower.go
+++ tolower.go
@@ -4,7 +4,7 @@ func ToLower(s string) string {
 	var str string
 	for _, letter := range s {
 		if letter > 64 && letter < 91 {
-			str = str + string(letter + 32)
+			str = str + string(letter+32)
 		} else {
 			str = str + string(letter)
 		}
